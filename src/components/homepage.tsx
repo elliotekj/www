e22c622a9da6442acc6a447b5ad49abb5ab801c0
Elliot Jackson <elliot@elliotekj.com>
2019-12-08 20:06:08 +0100

    Tighten up homepage spacing

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -36,7 +36,7 @@ const Homepage = ({ data }: Props) => {
 
   return (
     <Layout>
-      <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
+      <section sx={{ mb: [5, 6], p: { fontSize: [1, 2, 3], mt: 2 } }}>
         <Hero />
       </section>
       <Title text="Latest Posts">
