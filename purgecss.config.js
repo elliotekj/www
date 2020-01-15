8d88df1c5e2e6aacdbb4208808ab08cf8a4617c7
Elliot Jackson <elliot@elliotekj.com>
2020-01-15 19:01:19 +0100

    Templating updates

---
+++ purgecss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['.rendered/**/*.html'],
+    css: ['content/assets/styles.css'],
+    whitelist: [
+        'lg:w-9/12',
+        'lg:w-3/12',
+        'hover:text-white',
+    ]
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  theme: {
-    screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1024px',
+    theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1024px',
+        },
+        extend: {
+            colors: {
+                codeblock: '#090c10'
+            },
+        },
     },
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    variants: {},
+    plugins: []
 }
