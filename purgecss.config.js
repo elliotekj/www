b8a3c211f7997cf6b86d304aefe752f0ac13c8f1
Elliot Jackson <elliot@elliotekj.com>
2020-01-17 12:31:04 +0100

    Refactor css

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: ['.rendered/**/*.html'],
-    css: ['content/assets/styles.css'],
-    whitelist: [
-        'lg:w-9/12',
-        'lg:w-3/12',
-        'hover:text-white',
-    ]
-}
